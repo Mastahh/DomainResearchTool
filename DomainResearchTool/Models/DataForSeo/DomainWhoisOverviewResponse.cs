f83ae52c28f9e07b2937b46a974a4cba55fd2d2c
Oleg Kuznetsov <Oleg.Kuznetsov@riverty.com>
2023-04-19 22:34:14 +0300

    Added data retrieve from API

--- DomainResearchTool/Models/DataForSeo/DomainWhoisOverviewResponse.cs
+++ DomainResearchTool/Models/DataForSeo/DomainWhoisOverviewResponse.cs
@@ -8,30 +8,44 @@ namespace DomainResearchTool.Models.DataForSeo
         public string Version { get; set; } = string.Empty;
 
         [JsonProperty("status_code")]
-        public int StatisCode { get; set; }
+        public int StatusCode { get; set; }
 
         [JsonProperty("status_message")]
-        public string StatisMessage { get; set; } = string.Empty;
+        public string StatusMessage { get; set; } = string.Empty;
 
         [JsonProperty("tasks")]
         public List<TaskResponse> Tasks { get; set; }
 
         public bool IsError()
         {
-            var task = Tasks.FirstOrDefault();
-            if (task != null && task.StatusCode >= 40000)
+            if(StatusCode >= 40000)
             {
                 return true;
+            }
+            if (Tasks != null)
+            {
+                var task = Tasks.FirstOrDefault();
+                if (task != null && task.StatusCode >= 40000)
+                {
+                    return true;
+                }
             }
             return false;
         }
 
         public string GetErrorMessage()
         {
-            var task = Tasks.FirstOrDefault();
-            if (task != null && task.StatusCode >= 40000)
+            if (!string.IsNullOrWhiteSpace(StatusMessage))
             {
-                return task.StatusMessage;
+                return StatusMessage;
+            }
+            if (Tasks != null)
+            {
+                var task = Tasks.FirstOrDefault();
+                if (task != null && task.StatusCode >= 40000)
+                {
+                    return task.StatusMessage;
+                }
             }
             return string.Empty;
         }
